2328561eb6dd1581e767b16045c72471f01d067d
felix <fel93@live.fr>
2021-01-04 15:19:02 +0100

    frist commit web

---
+++ reactapp/src/Component/navbar.js
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText
+} from 'reactstrap';
+import {Link} from 'react-router-dom';
+
+const Example = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">Babies and Kids</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink>
+              <Link to="/Accueil">Acceuil</Link>
+              </NavLink>
+             
+            </NavItem>
+            <NavItem>
+              <NavLink>
+             <Link to='/Vente'>Vente</Link>
+             </NavLink>
+            </NavItem> 
+            <NavItem>
+             <NavLink> <Link to='/recherche'>Recherche</Link></NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Profil
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>
+                <NavLink ><Link to='/Achete'> Articles achetés</Link></NavLink>
+                </DropdownItem>
+                <DropdownItem>
+                <NavLink > <Link to='/Vendu'> Articles Vendus</Link></NavLink>
+                </DropdownItem>
+                <DropdownItem>
+                <NavLink><Link to='/information'>  Informations personelles</Link></NavLink>
+                </DropdownItem>
+                <DropdownItem>
+                  Porte feuille
+                </DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>
+                <NavLink href='/'> Log Out</NavLink>
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+          <NavbarText></NavbarText>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+}
+
+export default Example;
